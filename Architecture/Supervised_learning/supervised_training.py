604ab051c07c8d3c9deb3c251311aa95aab89b2a
CittiX <c.epifania5@studenti.uniba.it>
2025-01-14 20:28:32 +0100

    Introduced unavailability of drag and thrust physics constants

--- Architecture/Supervised_learning/supervised_training.py
+++ Architecture/Supervised_learning/supervised_training.py
@@ -18,6 +18,19 @@ training_conf["epochs"] = 3000
 training_conf["anim_record_freq"] = 10
 training_conf["learning_rate"] = 0.005
 
+# Initial guesses for parameters are provided
+init_drag_coef = 1
+init_thrust_coef = 1
+
+# Unknown parameters
+drag_coefficient = torch.tensor(init_drag_coef, dtype=torch.float, requires_grad=True)
+thrust_magnitude = torch.tensor(init_thrust_coef, dtype=torch.float, requires_grad=True)
+learnable_constants = [drag_coefficient, thrust_magnitude]
+
+# Scale factors
+constant_scale_factors = {"drag_coefficient": 1,
+                          "thrust_magnitude": 3000}
+
 # Reinitialize network, optimizer, input and target tensors
 model = nn.Sequential(
     nn.Linear(input_dim, hidden_dim),
@@ -27,7 +40,9 @@ model = nn.Sequential(
     nn.Linear(hidden_dim, output_dim)
 )
 
-optimizer = optim.Adam(model.parameters(), lr=training_conf["learning_rate"])
+optimizer = optim.Adam([{"params": model.parameters(), "lr": training_conf["learning_rate"]},
+                        {"params": learnable_constants, "lr": training_conf["learning_rate"] / 10}]
+                       )
 input_tensor = torch.tensor(inputs, dtype=torch.float).view(-1, 1)
 target_tensor = torch.tensor(targets, dtype=torch.float).view(-1, 2)
 
@@ -45,7 +60,7 @@ def train_one_epoch_supervised(model, inputs, targets, optimizer):
     mse_loss = criterion(predictions, targets)
 
     # Physics loss
-    physics_loss = compute_physics_loss(model, missile)
+    physics_loss = compute_physics_loss(model, learnable_constants, constant_scale_factors)
 
     # Combined loss
     loss = mse_loss + physics_loss
